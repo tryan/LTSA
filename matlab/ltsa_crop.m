60173f9a575965cb56caf9af33ad8948c66e1512
Ryan Kok <ryant8@gmail.com>
2012-02-11 23:35:56 -0800

    Minor documentation changes

--- matlab/ltsa_crop.m
+++ matlab/ltsa_crop.m
@@ -1,9 +1,17 @@
 function ltsa = ltsa_crop(ltsa, fs, duration, freq_range, time_range)
 
+% crops an LTSA to a time and/or frequency subset
+%
+% duration in samples
+% freq_range in Hertz, ie [0 20000]
+% time_range in seconds, ie [200 500]
+
 y_range = round( freq_range ./ (fs/2) * size(ltsa, 1) );
+x_range = round( time_range ./ duration * fs * size(ltsa, 2) );
+
+% check for out of bounds indices and fix
 y_range(y_range < 1) = 1;
 y_range(y_range > size(ltsa, 1)) = size(ltsa, 1);
-x_range = round( time_range ./ duration * fs * size(ltsa, 2) );
 x_range(x_range < 1) = 1;
 x_range(x_range > size(ltsa, 2)) = size(ltsa, 2);
 
--- matlab/ltsa_run.m
+++ matlab/ltsa_run.m
@@ -6,7 +6,7 @@ clear; clc; close all;
 
 % name of file to process, either provide the full file path or
 % change the matlab/octave directory to the containing folder
-file = '~/trains.wav';
+file = 'C:\numb.wav';
 
 % file is read in
 [sig fs] = wavread(file);
@@ -14,18 +14,20 @@ sig = single( sig(:, 1) );
 nsamples = length(sig);
 
 % LTSA configuration
-div_len = .5 * fs;
-subdiv_len = floor(div_len/4);
+div_len = round(.5 * fs);
+subdiv_len = floor(div_len/6);
 nfft = subdiv_len;
-noverlap = subdiv_len/2;
+noverlap = round( subdiv_len/4 );
 
+tic
 ltsa = ltsa_process(sig, div_len, subdiv_len, noverlap, nfft);
+toc
 
 % conversion to double necessary for octave's imagesc() not to crash
-ltsa = double(ltsa);
+% ltsa = double(ltsa);
 
 % display LTSA
 figure;
-ltsa_view(double(ltsa), fs, nsamples);
+ltsa_view(ltsa, fs, nsamples, [0 15000]);
 
 clear sig;
