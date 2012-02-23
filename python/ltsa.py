f4bd07e465f9397f9b96ebeb023bae3485159356
Thomas Kok <ryant8@gmail.com>
2012-02-22 22:53:53 -0800

    Added docstrings to LTSA class methods.
    Cleaned up LTSA.show() input handling

--- python/ltsa.py
+++ python/ltsa.py
@@ -3,7 +3,6 @@ import numpy as np
 from numpy.fft import rfft
 from scipy.io.wavfile import read as wavread
 import matplotlib.pyplot as plt
-from scipy.signal import decimate
 from scipy.misc import imresize
 
 class LTSA():
@@ -19,17 +18,10 @@ class LTSA():
         # defaults for user adjustable values
         self.div_len = np.round(self.fs/2) # one second divisions
         self.subdiv_len = 2**np.round(np.log2(self.fs/5))
-#        self.subdiv_len = np.round(self.div_len / 4.0)
-#        self.subdiv_len += self.subdiv_len % 2
         self.nfft = self.subdiv_len
         self.noverlap = 0
 
-        # useful values
-        self.nsamples = np.size(self.raw, 0)
-        self.ndivs = np.floor(self.nsamples / self.div_len)
-        self.nsubdivs = np.floor(self.div_len / (self.subdiv_len - self.noverlap))
-
-        # plotting values
+        # time and frequency limits, used for displaying results
         self.tmin = 0
         self.tmax = np.floor(self.nsamples / self.fs)
         self.fmin = 0
@@ -37,15 +29,35 @@ class LTSA():
 
 # takes a dict of attribute name/value pairs and sets LTSA params accordingly
     def set_params(self, var_dict):
+        '''
+        Allows the user to set custom values for computation parameters.
+
+        It is recommended that only these variables be manipulated:
+            div_len
+            subdiv_len
+            nfft
+            noverlap
+
+        Input should be a dictionary, for example:
+        var_dict = {'div_len':8192, 'subdiv_len':1024}
+        '''
         for key, val in var_dict.iteritems():
             vars(self)[key] = val
 
     def crop(self, tmin = 0, tmax = -1, fmin = 0, fmax = -1):
-        if tmax == -1:
+        '''
+        Crop the computed LTSA in time and/or frequency
+
+        Input times should be given in seconds, frequencies in Hertz
+
+        Cropping the LTSA throws away any data that is cropped out
+        '''
+        if tmax < 0:
             tmax = self.nsamples / self.fs
-        if fmax == -1:
+        if fmax < 0:
             fmax = self.fs / 2
 
+# update time and frequency limits
         self.tmin = tmin
         self.tmax = tmax
         self.fmin = fmin
@@ -65,29 +77,56 @@ class LTSA():
 #        print div_low, div_high, freq_low, freq_high
         
     def show(self, resize=None):
-        if resize is not None:
+        '''
+        Displays the LTSA image in the current axis using
+        matplotlib.pyplot.imshow()
+
+        If no resize input is given, the self.ltsa is displayed without
+        modification
+
+        If resize is a tuple of two ints, scipy.misc.imresize is called:
+        imresize(self.ltsa, resize)
+
+        If resize is an int, the ltsa data is downsampled to a height of resize
+        '''
+        if isinstance(resize, tuple):
             img = imresize(self.ltsa, resize)
+        elif isinstance(resize, int):
+#           downsample (without lowpass filtering)
+            h = resize # img height in pixels
+            idx = np.floor(np.linspace(0, np.size(self.ltsa, 0)-1, h))
+            idx = np.int32(idx)
+            img = np.zeros((h, np.size(self.ltsa, 1)))
+            for i in xrange(int(np.size(self.ltsa, 1))):
+                img[:,i] = self.ltsa[idx,i] 
         else:
-#           downsample without lowpass filtering
-            h = 800 # img height in pixels
-            if h < np.size(self.ltsa, 0):
-                idx = np.floor(np.linspace(0, np.size(self.ltsa, 0)-1, h))
-                idx = np.int32(idx)
-                img = np.zeros((h, np.size(self.ltsa, 1)))
-                for i in xrange(int(np.size(self.ltsa, 1))):
-                    img[:,i] = self.ltsa[idx,i] 
-            else:
-                img = self.ltsa
+            img = self.ltsa
+
         self.handle = plt.imshow(img, origin='lower')
-        plt.xlabel('Time')
-        plt.ylabel('Frequency')
-        xtick_loc, tmp = plt.xticks()
-        xtick_loc = filter(lambda(x): x >= 0 and x <= np.size(self.ltsa, 1), xtick_loc)
+        plt.xlabel('Time (seconds)')
+        plt.ylabel('Frequency (Hertz)')
+
+# set time axis ticks & labels
+        xtick_loc, xtick_lbl = plt.xticks() # value of xtick_lbl will not be used
+        xtick_loc = xtick_loc[(xtick_loc >= 0) * (xtick_loc <= np.size(self.ltsa, 1))]
         xlbl_vals = np.round(np.linspace(self.tmin, self.tmax, len(xtick_loc)))
         xtick_lbl = [str(int(x)) for x in xlbl_vals]
         plt.xticks(xtick_loc, xtick_lbl)
 
+# set frequency axis ticks & labels
+        ytick_loc, ytick_lbl = plt.yticks() # value of ytick_lbl will not be used
+        ytick_loc = ytick_loc[(ytick_loc >= 0) * (ytick_loc <= np.size(self.ltsa, 0))]
+        ylbl_vals = np.round(np.linspace(self.fmin, self.fmax, len(ytick_loc)))
+        ytick_lbl = [str(int(y)) for y in ylbl_vals]
+        plt.yticks(ytick_loc, ytick_lbl)
+
     def compute(self): 
+
+        # useful values
+        self.nsamples = len(self.raw)
+        self.ndivs = np.floor(self.nsamples / self.div_len)
+        self.nsubdivs = np.floor(self.div_len / (self.subdiv_len - self.noverlap))
+
         self.raw = self.raw[: self.ndivs * self.div_len]
         self.ltsa = np.zeros((self.nfft/2, self.ndivs), dtype=np.single)
         divs = np.reshape(self.raw, (self.ndivs, self.div_len)).T
@@ -115,6 +154,9 @@ class LTSA():
 
         spectrum = np.single(np.log(spectrum / self.nsubdivs))
         return spectrum
+
+    def _tighten_color_axis(self, vmin, vmax):
+        spread = vmin - vmax
 
 
     def scale_to_uint8(self): pass
