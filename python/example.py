8ba38d54a6f03118fa398cc8fb34f999848fa88f
Ryan Kok <ryant8@gmail.com>
2012-02-25 09:51:51 -0800

    Updated example python script

--- python/example.py
+++ python/example.py
@@ -3,8 +3,12 @@ from ltsa import LTSA
 
 s = LTSA('/home/ryan/gun.wav')
 
-params = {}
+params = {'div_len': 22050,
+          'subdiv_len': 4096,
+          'nfft': 4096
+          'noverlap': 1000}
 
+s.set_params(params)
 s.compute()
 s.crop(fmax=16000)
 s.show(resize=800)
