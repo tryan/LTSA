23993f6f800da552f8b9b9916461326be6ad82d4
Thomas Kok <ryant8@gmail.com>
2012-03-04 19:11:29 -0800

    Updated example.py

--- python/example.py
+++ python/example.py
@@ -1,15 +1,15 @@
 from matplotlib.pyplot import show
-from ltsa import LTSA
+from ltsa import WavLTSA, RawLTSA
 
-s = LTSA('/home/ryan/gun.wav')
+s = WavLTSA('/home/ryan/gun.wav')
 
 params = {'div_len': 22050,
           'subdiv_len': 4096,
-          'nfft': 4096
+          'nfft': 4096,
           'noverlap': 1000}
 
 s.set_params(params)
 s.compute()
 s.crop(fmax=16000)
-s.show(resize=800)
+s.show()
 show()
