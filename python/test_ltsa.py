34656f490853e58bf8850cb6b550e3b24d3ad60c
Thomas Kok <ryant8@gmail.com>
2012-03-04 21:00:48 -0800

    Updated test suite for crop()

--- python/test_ltsa.py
+++ python/test_ltsa.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import unittest as ut
 import numpy as np
 from ltsa import *
@@ -41,6 +42,15 @@ class TestLTSA(ut.TestCase):
     def test_crop(self):
         self.setUp()
 
+        # in this test case we want to crop the middle half of both the time
+        # and frequency ranges
+
+        fs = self.gram.fs
+        ndivs = self.gram.ndivs
+        div_len = self.gram.div_len
+        divs_per_second = fs / div_len
+        pixels_per_hz = self.gram.ltsa.shape[0] / (fs/2)
+
         tmax = self.gram.tmax
         tmax_new = int(tmax * 3.0/4.0)
         tmin_new = int(tmax * 1.0/4.0)
@@ -49,7 +59,15 @@ class TestLTSA(ut.TestCase):
         fmax_new = int(fmax * 3.0/4.0)
         fmin_new = int(fmax * 1.0/4.0)
 
-        self.gram.crop(tmin_new, tmax_new, fmin_new, fmax_new)
+        div_low = np.floor(tmin_new * divs_per_second)
+        div_high = np.floor(tmax_new * divs_per_second) + 1
+        freq_low = np.floor(fmin_new * pixels_per_hz)
+        freq_high = np.ceil(fmax_new * pixels_per_hz) + 1
+        expected_results = (div_low, div_high, freq_low, freq_high)
+
+        crop_results = self.gram.crop(tmin_new, tmax_new, fmin_new, fmax_new)
+
+        self.assertEqual(crop_results, expected_results)
 
 def suite():
     suite = ut.TestSuite()
