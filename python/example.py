c08309a7e78610c4412a121bd77b480d91c3cc1f
Thomas Kok <ryant8@gmail.com>
2012-03-08 11:16:24 -0800

    Updated example.py

--- python/example.py
+++ python/example.py
@@ -1,15 +1,25 @@
-from matplotlib.pyplot import show
-from ltsa import WavLTSA, RawLTSA
+from matplotlib.pyplot import show, colorbar
+from ltsa import WavLTSA
 
-s = WavLTSA('/home/ryan/gun.wav')
+# initialize the object with raw data from wav file
+s = WavLTSA('/home/ryan/trains.wav')
 
+# some reasonable settings for a 44.1kHz sampling rate
 params = {'div_len': 22050,
           'subdiv_len': 4096,
           'nfft': 4096,
           'noverlap': 1000}
 
+# apply the parameters
 s.set_params(params)
-s.compute()
-s.crop(fmax=16000)
+
+# compute the LTSA -- identical to s.compute()
+s()
+
+# throw out data over 6kHz as there isn't much interesting activity there
+# in a typical song
+s.crop(fmax=6000)
 s.show()
+
+colorbar()
 show()
